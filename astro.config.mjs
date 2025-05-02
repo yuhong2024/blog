9ff3c396c3bfc92f6fda7c3568d28c1a9758cebf
yuhong2024 <wyhstar@email.swu.edu.cn>
2025-05-02 18:55:50 +0800

    1

--- astro.config.mjs
+++ astro.config.mjs
@@ -20,7 +20,7 @@ import { remarkReadingTime } from "./src/plugins/remark-reading-time.mjs";
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://fuwari.vercel.app/",
+	site: "https://echooler.com/",
 	base: "/",
 	trailingSlash: "always",
 	integrations: [
--- src/config.ts
+++ src/config.ts
@@ -7,9 +7,9 @@ import type {
 import { LinkPreset } from "./types/config";
 
 export const siteConfig: SiteConfig = {
-	title: "Fuwari",
-	subtitle: "Demo Site",
-	lang: "en", // 'en', 'zh_CN', 'zh_TW', 'ja', 'ko', 'es', 'th'
+	title: "Echooler",
+	subtitle: "Y.Hong's Blog",
+	lang: "zh_CN", // 设置为中文
 	themeColor: {
 		hue: 250, // Default hue for the theme color, from 0 to 360. e.g. red: 0, teal: 200, cyan: 250, pink: 345
 		fixed: false, // Hide the theme color picker for visitors
@@ -28,14 +28,7 @@ export const siteConfig: SiteConfig = {
 		enable: true, // Display the table of contents on the right side of the post
 		depth: 2, // Maximum heading depth to show in the table, from 1 to 3
 	},
-	favicon: [
-		// Leave this array empty to use the default favicon
-		// {
-		//   src: '/favicon/icon.png',    // Path of the favicon, relative to the /public directory
-		//   theme: 'light',              // (Optional) Either 'light' or 'dark', set only if you have different favicons for light and dark mode
-		//   sizes: '32x32',              // (Optional) Size of the favicon, set only if you have favicons of different sizes
-		// }
-	],
+	favicon: [],
 };
 
 export const navBarConfig: NavBarConfig = {
@@ -45,33 +38,61 @@ export const navBarConfig: NavBarConfig = {
 		LinkPreset.About,
 		{
 			name: "GitHub",
-			url: "https://github.com/saicaca/fuwari", // Internal links should not include the base path, as it is automatically added
-			external: true, // Show an external link icon and will open in a new tab
+			url: "https://github.com/yuhong2024",
+			external: true,
+		},
+		{
+			name: "知乎",
+			url: "https://www.zhihu.com/people/zhai-xing-3-26",
+			external: true,
+		},
+		{
+			name: "YouTube",
+			url: "https://studio.youtube.com/channel/UCRVaRv7mraTBpHmtPO3i9vw",
+			external: true,
+		},
+		{
+			name: "Bilibili",
+			url: "https://space.bilibili.com/1554305035",
+			external: true,
+		},
+		{
+			name: "学术主页",
+			url: "https://yuhong2024.github.io/index.html",
+			external: true,
 		},
 	],
 };
 
 export const profileConfig: ProfileConfig = {
 	avatar: "assets/images/demo-avatar.png", // Relative to the /src directory. Relative to the /public directory if it starts with '/'
-	name: "Lorem Ipsum",
-	bio: "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+	name: "Echooler",
+	bio: "Y.Hong 本科生 SWU",
 	links: [
-		{
-			name: "Twitter",
-			icon: "fa6-brands:twitter", // Visit https://icones.js.org/ for icon codes
-			// You will need to install the corresponding icon set if it's not already included
-			// `pnpm add @iconify-json/<icon-set-name>`
-			url: "https://twitter.com",
-		},
-		{
-			name: "Steam",
-			icon: "fa6-brands:steam",
-			url: "https://store.steampowered.com",
-		},
 		{
 			name: "GitHub",
 			icon: "fa6-brands:github",
-			url: "https://github.com/saicaca/fuwari",
+			url: "https://github.com/yuhong2024",
+		},
+		{
+			name: "知乎",
+			icon: "fa6-brands:zhihu",
+			url: "https://www.zhihu.com/people/zhai-xing-3-26",
+		},
+		{
+			name: "YouTube",
+			icon: "fa6-brands:youtube",
+			url: "https://studio.youtube.com/channel/UCRVaRv7mraTBpHmtPO3i9vw",
+		},
+		{
+			name: "Bilibili",
+			icon: "fa6-brands:bilibili",
+			url: "https://space.bilibili.com/1554305035",
+		},
+		{
+			name: "学术主页",
+			icon: "fa6-solid:graduation-cap",
+			url: "https://yuhong2024.github.io/index.html",
 		},
 	],
 };
